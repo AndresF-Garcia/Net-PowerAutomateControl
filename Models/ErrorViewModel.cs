c7d9e77cabe2773ad45d16ac54709be5493001d0
Andres <naranjoaf01@gmail.com>
2024-09-14 20:11:55 -0500

    Inicialización
    Primer commit del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PowerAutomateControl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
